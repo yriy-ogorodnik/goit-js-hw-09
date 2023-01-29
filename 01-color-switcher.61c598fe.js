2a44957ec9540b01a796b4f984d84881e996708a
yriy-ogorodnik <96260784+yriy-ogorodnik@users.noreply.github.com>
2023-01-29 17:21:40 +0000

    Deploying to gh-pages from @ yriy-ogorodnik/goit-js-hw-09@f66920810a2a914ee78679f7231c6f3eb4e41b1c 🚀

---
+++ 01-color-switcher.61c598fe.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0;let d=null;t.addEventListener("click",(()=>{t.disabled=!0,e.disabled=!1,d=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(()=>{t.disabled=!1,e.disabled=!0,clearInterval(d),console.log(`Interval with id ${d} has stopped!`)}));
+//# sourceMappingURL=01-color-switcher.61c598fe.js.map
